ff1256cecf2666e095e84026824c82bd24914f49
bhanu2507 <bhanureddy@gmail.com>
2017-08-28 19:38:33 +0530

    class image

--- server/db.js
+++ server/db.js
@@ -3,9 +3,9 @@
 
 module.exports={
 	db: {
-		host : '104.196.119.84',
+		host : '130.211.245.39',
 		user : 'root',
-		password : 'root',
-		database : 'yogaalliance'
+		password : 'yaikriyoga',
+		database : 'yai'
 	}
 };
